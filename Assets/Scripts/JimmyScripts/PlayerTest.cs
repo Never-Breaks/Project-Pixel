5c0d3265e25aeab23945071a9168ea521a39d9f4
jmcgurn <jmcgurn@email.neit.edu>
2021-06-19 10:31:53 -0400

    working on player and action map controls

--- Assets/Scripts/JimmyScripts/PlayerTest.cs
+++ Assets/Scripts/JimmyScripts/PlayerTest.cs
@@ -6,13 +6,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerTest : MonoBehaviour
 {
+    public float speed = 10.0f;
     public Vector2 moveVal;
     public Vector2 lookVal;
-    public bool isJumping = false; 
+    public float aimVal;
+    public PlayerInput playerInput;
+    public enum PlayerState {Default, Aiming, Jumping}
+    public PlayerState playerState;
+    public CharacterController cc;
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
+        cc = GetComponent<CharacterController>();
+        playerState = PlayerState.Default;
+        playerInput = GetComponent<PlayerInput>();
     }
 
     public void OnMove(InputValue value)
@@ -27,25 +36,119 @@ public class PlayerTest : MonoBehaviour
 
     public void OnJump (InputValue value)
     {
-        if (!isJumping)
-        {
-            isJumping = value.isPressed;
-        }
+        //add force on rigid body to make player jump
+
+        //switch controls to jump mode
+        playerInput.SwitchCurrentActionMap("PlayerJump");
+
+        //switch playerstate to jumping
+        playerState = PlayerState.Jumping;
+    }
+
+    public void OnAim(InputValue value)
+    {
+        //update aim value
+        aimVal = value.Get<float>();
+    }
+
+    public void OnFire(InputValue value)
+    {
+        Debug.Log("fire");
     }
 
     // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        switch(playerState)
+        {
+            case PlayerState.Default:
+                #region Player Movement
+
+                Vector3 direction = new Vector3(moveVal.x, 0, moveVal.y);
+                Vector3 movement = transform.TransformDirection(direction) * speed;
+                isGrounded = cc.SimpleMove(movement);
+
+                #endregion
+                break;
+            case PlayerState.Jumping:
+
+
+                break;
+            case PlayerState.Aiming:
+                break;
+        }
+    }
+
     void Update()
     {
-        if(isJumping)
+       // Debug.Log(playerInput.currentActionMap);
+
+        switch (playerState)
         {
-            CheckForGround();
+            case PlayerState.Default:
+                Debug.Log(playerInput.currentActionMap);
+
+            
+
+                //if the aim button is fuly pressed
+                if (aimVal >= 1f)
+                {
+                    //switch cinemachine cam to aiming cam
+
+                    //switch controls to aim mode
+                    playerInput.SwitchCurrentActionMap("PlayerAim");
+
+                    //switch playerstate to aiming
+                    playerState = PlayerState.Aiming;
+                }
+                break;
+
+            case PlayerState.Jumping:
+
+                #region Player Movement
+
+                #endregion
+                bool isGrounded = true;
+                Debug.Log(isGrounded);
+
+                //if the player has grounded
+                if (isGrounded)
+                {
+                    //switch controls to default mode
+                    playerInput.SwitchCurrentActionMap("PlayerMove");
+
+                    //switch playerstate to default
+                    playerState = PlayerState.Default;
+                }
+                break;
+
+            case PlayerState.Aiming:
+
+                #region Player Movement
+
+                #endregion
+
+                //if the aim button is released
+                if (aimVal < 0.5f)
+                {
+                    //switch cinemachine cam to default cam
+
+                    //switch controls to default mode
+                    playerInput.SwitchCurrentActionMap("PlayerMove");
+
+                    //switch playerstate to default
+                    playerState = PlayerState.Default;
+                }
+                break;       
         }
     }
 
     public bool CheckForGround()
     {
-        //cast aray to see if we are in the air or on he ground
+        //cast a ray to see if we are in the air or on the ground
 
+        //if ray hits ground return true
         return true;
     }
 }
