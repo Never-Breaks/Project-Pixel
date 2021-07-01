e83dc8b644537d7ebf3ce5a62879650eaf644b09
Brandon <Blabrake1@gmail.com>
2021-07-01 17:35:02 -0400

    Exp system + test level

---
+++ Assets/Scripts/BrandonScripts/ExpScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpScript : MonoBehaviour
+{
+    int level = 0;
+    public static int exp;
+    int lastexpValue;
+    [SerializeField] int expToNextLevel = 20;
+    int overFillExp;
+    ExpBar Bar;
+
+    void Update()
+    {
+        if (exp >= expToNextLevel)
+        {
+            LevelUp();
+        }
+        if (exp != lastexpValue)
+        {
+            GainExp();
+        }
+    }
+    //call this whenever you want the player to gain exp
+    void GainExp()
+    {
+        Bar.UpdateValue(exp);
+        lastexpValue = exp;
+    }
+    void LevelUp()
+    {
+        level++;
+        CheckForExpOverFill();
+        //this makes it so levels will take longer and longer to achive
+        expToNextLevel = (int)Mathf.Round(expToNextLevel * 1.5f);
+        exp = overFillExp;
+        Bar.newMax(expToNextLevel, exp);
+    }
+    void CheckForExpOverFill()
+    {
+        overFillExp = exp - expToNextLevel;
+    }
+}
